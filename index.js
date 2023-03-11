3aa415c88c0f3a35605df838c6bb6a7cbcf62544
jelizasilatei <silateijc90@gmail.com>
2023-03-11 13:02:00 +0300

    created arithmetic functions in JS

---
+++ index.js
@@ -0,0 +1,33 @@
+//basic math fucntions
+function add(x,y){
+    return x+y;
+}
+
+function subtract(x,y){
+    return x-y;
+}
+
+function multiply(x,y){
+    return x*y;
+}
+
+function divide(x,y){
+    return x/y;
+}
+
+function increment(n){
+    return(n += 1);
+}
+
+function decrement(n){
+    return(n -= 1);
+}
+
+
+function makeInt(num){
+    return parseInt(num, 10);
+}
+
+function preserveDecimal(num){
+    return parseFloat(num);
+}
